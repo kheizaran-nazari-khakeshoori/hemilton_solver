e7cbb609615e9bbc509ae5ac402695dd6d5a9894
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-03-19 14:09:24 +0100

    add tabu types

--- tabu_search.py
+++ tabu_search.py
@@ -11,3 +11,17 @@ from hemiltonian_energy import hamiltonian_vectorized
 
 
 Move = Tuple[int, int]
+
+
+@dataclass
+class TabuResult:
+	best_spins: np.ndarray
+	best_energy: float
+	final_spins: np.ndarray
+	final_energy: float
+	history_best_energy: np.ndarray
+	accepted_worse_moves: int
+
+
+def _canonical_move(i: int, j: int) -> Move:
+	return (i, j) if i < j else (j, i)
